75c51a1152230e8c74e99ced84c57d05e376326c
ngonimombeshora <nmombeshora3@gmail.com>
2020-10-07 14:01:13 +0200

    added first class to be tested

--- setup.py
+++ setup.py
@@ -4,19 +4,23 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="homehubRPi",  # Replace with your own username
+    name="homehubRPi",
     version="0.0.1",
     author="Ngoni Mombeshora",
     author_email="nmombeshora3@gmail.com",
     description="A small home hub example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ngonimombeshora/homehubRPi",
+    # this should get the tar.gz available
+    download_url='https://github.com/ngonimombeshora/homehubRPi/tree/main/dist/homehubRPi-0.0.1.tar.gz',
+    keywords=['Smart home', 'IOT', 'Raspberry Pi', 'Library', 'pip3'],
     packages=setuptools.find_packages(),
     classifiers=[
+        'Intended Audience :: Developers',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Linux",
     ],
     python_requires='>=3.6',
 )
